e5a766aa6b119cb96f2eb03e0962610d398ce8cd
Alexander Berezhnev <freelancerbav@gmail.com>
2023-02-01 09:06:01 +0300

    finished a registration

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,7 +4,13 @@ export async function registration(req, res) {
 	try {
 		const { username, email, password } = req.body;
 		const response = await registrationService(username, email, password);
-		res.status(201).json(response);
+
+		res.cookie('refreshToken', response.refreshToken, {
+			maxAge: 30 * 24 * 3600 * 1000,
+			httpOnly: true,
+			secure: true,
+		});
+		res.status(201).json({ ...response, refreshToken: undefined });
 	} catch (error) {
 		console.log(error);
 		res.status(401).json('Непредвиденная ошибка');
