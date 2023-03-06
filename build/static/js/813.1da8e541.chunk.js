94209cec2c6d2fd49453ad99138c5558f30d353f
Alexander Berezhnev <freelancerbav@gmail.com>
2023-03-06 09:00:42 +0300

    lazy loading for the admin block

---
+++ build/static/js/813.1da8e541.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[813],{813:function(e,n,i){i.r(n),i.d(n,{default:function(){return c}});i(2791);var s={img:"AdminBG_img__ALteJ"},a=i(184),c=function(){return(0,a.jsx)("section",{className:s.wrapper,children:(0,a.jsx)("img",{className:s.img,src:"images/admin-bg-small.png",alt:"bg"})})}}}]);
+//# sourceMappingURL=813.1da8e541.chunk.js.map
