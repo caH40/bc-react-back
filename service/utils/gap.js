817e00053c20326fe4edc42603946961e75ea64d
Alexander Berezhnev <freelancerbav@gmail.com>
2023-03-12 09:32:34 +0300

    fixed a bug with gaps

--- service/utils/gap.js
+++ service/utils/gap.js
@@ -1,6 +1,12 @@
 export function gapValue(results) {
 	try {
 		//вычисление отставаний
+
+		results.sort((a, b) => {
+			if (typeof b.time !== 'number') return -1; //игнорирование отсутствие результата
+			return a.time - b.time;
+		});
+
 		const lengthResult = results.length;
 		for (let i = 1; i < lengthResult; i++) {
 			results[i].gap = results[i].time - results[0].time;
